74c887aa73ee597043652261ed006629c0895990
Luladay <33706585+Luladay@users.noreply.github.com>
2018-12-03 16:24:08 -0800

    Added LP's util functions

--- src/util.py
+++ src/util.py
@@ -1,5 +1,11 @@
 import pandas as pd
 import numpy as np
+import pickle
+import itertools
+import matplotlib
+matplotlib.use('agg')
+import matplotlib.pyplot as plt
+from sklearn.metrics import confusion_matrix
 
 
 def get_country_mapping():
@@ -11,3 +17,34 @@ def get_country_mapping():
         code = table['CCode'].iloc[i]
         codes_to_names[code] = name
     return codes_to_names
+
+def dumpVar(filename, obj):
+	with open(filename, 'wb') as f:
+		pickle.dump(obj, f, pickle.HIGHEST_PROTOCOL)
+
+def openPkl(filename):
+	var = open(filename, "rb")
+	return pickle.load(var)
+
+classes = np.asarray(list(range(1,6)))
+
+def outputConfusionMatrix(pred, labels, filename):
+    """ Generate a confusion matrix """
+    
+    cm = confusion_matrix(labels, pred, labels=classes)
+    plt.figure()
+    plt.imshow(cm, interpolation='nearest', cmap=plt.cm.Reds)
+    plt.colorbar()
+
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes)
+    plt.yticks(tick_marks, classes)
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, cm[i, j],
+                 horizontalalignment="center",
+                 color="white" if cm[i, j] > thresh else "black")
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+    plt.savefig(filename)   
